adfbcb5577708a14d39e19bbf539e1e204304600
bienonygus <bienkowskisz@gmail.com>
2020-03-31 14:23:25 +0200

    Pierwszy program testowy

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+  GNU nano 4.3                                                     HelloWord.java                                                                     
+public class HelloWorld {
+
+                public static void main(String[] args {
+                        //Prints "Hello World" to the terminal
+                                System.out.println("Hello World");
+        }
+}
+
